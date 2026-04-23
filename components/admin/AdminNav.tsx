8b7bf74f0626783bec82f937dbc099e5e182586a
Nsisong Effiong <nsisongeffiong@outlook.com>
2026-04-23 04:58:08 -0400

    feat: add About page link to AdminNav

--- components/admin/AdminNav.tsx
+++ components/admin/AdminNav.tsx
@@ -9,6 +9,7 @@ const navItems = [
   { label: 'Dashboard', href: '/admin/dashboard' },
   { label: 'Posts', href: '/admin/posts' },
   { label: 'Comments', href: '/admin/comments' },
+  { label: 'About page', href: '/admin/about' },
 
 ];
 
